ec71e183124756be0585785d22abdc67951646f7
Maxim Fateev <mfateev@gmail.com>
2025-05-09 20:24:32 -0700

    Removed client related activity.

--- openai_agents/run_worker.py
+++ openai_agents/run_worker.py
@@ -1,17 +1,13 @@
 from __future__ import annotations
 
 import asyncio
-import concurrent.futures
 
 from temporalio.client import Client
 from temporalio.worker import Worker
 
-from agents import TResponseInputItem
-
 from openai_agents.adapters._open_ai_converter import open_ai_data_converter
-from openai_agents.adapters.invoke_model_activity import invoke_open_ai_client, invoke_open_ai_model
+from openai_agents.adapters.invoke_model_activity import invoke_open_ai_model
 from openai_agents.workflows.agents_as_tools_workflow import AgentsAsToolsWorkflow
-from openai_agents.workflows.get_weather_activity import get_weather
 from openai_agents.workflows.customer_service_workflow import CustomerServiceWorkflow
 from openai_agents.workflows.get_weather_activity import get_weather
 from openai_agents.workflows.hello_world_workflow import HelloWorldAgent
@@ -30,7 +26,7 @@ async def main():
         task_queue="my-task-queue",
         workflows=[HelloWorldAgent, ToolsWorkflow, ResearchWorkflow, CustomerServiceWorkflow,
                    AgentsAsToolsWorkflow],
-        activities=[invoke_open_ai_client, invoke_open_ai_model, get_weather],
+        activities=[invoke_open_ai_model, get_weather],
         # activity_executor=activity_executor,
     )
     await worker.run()
